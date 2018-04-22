8b101e97f6f8981767b42dbb4c029b81c67b62ac
huangweichuang <754008802@qq.com>
2018-04-22 16:03:09 +0800

    提交pinyougou项目

---
+++ pinyougou/pinyougou-dao/src/main/java/com/pinyougou/mapper/BrandMapper.java
@@ -0,0 +1,15 @@
+package com.pinyougou.mapper;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * Date：2018/4/20
+ * Author：Chuang
+ * Desc：
+ */
+public interface BrandMapper {
+
+    public List<TbBrand> queryAll();
+}
---
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,15 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * Date：2018/4/20
+ * Author：Chuang
+ * Desc：
+ */
+public interface BrandService {
+
+    public List<TbBrand> queryAll();
+}
